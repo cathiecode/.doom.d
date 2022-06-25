23223142e930762398ad239894a0e88d6268a4ac
cathiecode <cathiecode@gmail.com>
2022-06-26 07:51:42 +0900

    Initial commit

---
+++ custom.el
@@ -0,0 +1,21 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(neotree)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; auto save
+(auto-save-buffers-enhanced t)
+(setq auto-save-buffers-enhanced-interval 2)
+(setq auto-save-buffers-enhanced-quiet-save-p nil)
+(setq auto-save-default nil)
+
+;; windmove
+(windmove-default-keybindings)
